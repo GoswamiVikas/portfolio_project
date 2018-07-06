a513e0ce9112aa01cc7cf7bcfd5ea7bba00e7357
Vikas Goswami <vikas.mca17.du@gmail.com>
2018-07-06 14:06:10 +0530

    blog model up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Create blog model
+##title/pubdate/body/image
+
+#add blog app to settings
+#create a migrations
+#migrate
+#add to the admin
+
+class Blog(models.Model):
+	title = models.CharField(max_length=255)
+	pubdate = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
