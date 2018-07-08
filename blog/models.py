fd2a6ce1a3ead4f26f14fa8059ebd90d67e897cc
Vikas Goswami <vikas.mca17.du@gmail.com>
2018-07-08 17:50:25 +0530

    project prototype up and running

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,12 @@ class Blog(models.Model):
 	pubdate = models.DateTimeField()
 	body = models.TextField()
 	image = models.ImageField(upload_to='images/')
+
+	def summary(self):
+		return self.body[:50]
+
+	def date(self):
+		return self.pubdate.strftime('%b %e %Y')
+
+	def __str__(self):
+		return self.title
